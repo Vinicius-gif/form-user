10b68f994645f668408b790e958c684aa7ff5604
Vinicius-gif <vfpv@hotmail.com>
2023-11-20 15:25:45 -0300

    update

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -2,24 +2,41 @@
 
 import React from 'react'
 import { useForm } from 'react-hook-form'
-import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod';
+import { date, z } from 'zod'
 
 const schema = z.object({
-  name: z.string(),
+  name: z.string().min(3).max(50),
   document: z.string(),
   email: z.string().email('Email é inválido'),
   initialDate: z.string(),
   finalDate: z.string()
+})
+.superRefine((fields, ctx) => {
+  if (fields.initialDate && !fields.finalDate) {
+    ctx.addIssue({
+      path: ['finalDate'],
+      code: z.ZodIssueCode.invalid_date,
+      message: 'Selecione uma data final'
+    })
+  }
+
+  if ( new Date (fields.initialDate).getTime > new Date(fields.finalDate).getTime) {
+    ctx.addIssue({
+      path: ['finalDate'],
+      code: z.ZodIssueCode.invalid_date,
+      message: 'A data final deve ser maior do que a data inicial'
+    })
+  }
 })
 
 type FormData = z.infer<typeof schema>
 
 const Form = () => {
   const { handleSubmit, register, formState: {errors} } = useForm<FormData>({
-    mode: 'onBlur'
+    mode: 'onBlur',
+    resolver: zodResolver(schema)
   });
-
-  console.log(errors);
 
   return (
     <div className="flex w-full flex-col items-center">
@@ -29,21 +46,39 @@ const Form = () => {
       onSubmit={handleSubmit((data) => console.log(data))}
       className='mt-3 flex w-full max-w-lg flex-col gap-2'
       >
-        <input 
-          {...register('name')} 
-          className='rounded-md px-3 py-1' 
-          placeholder='Name' 
-        />
-        <input 
-          {...register('document')} 
-          className='rounded-md px-3 py-1' 
-          placeholder='Document' 
-        />
-        <input 
-          {...register('email')} 
-          className='rounded-md px-3 py-1' 
-          placeholder='Email' 
-        />
+        <div className='flex w-full flex-col'>
+          <input 
+            {...register('name')} 
+            className='rounded-md px-3 py-1' 
+            placeholder='Name' 
+            />
+          {errors.name && (
+            <p className='text-red-500'>{errors.name.message}</p>
+            )}
+          </div>
+
+          <div className='flex w-full flex-col'>
+            <input 
+              {...register('document')} 
+              className='rounded-md px-3 py-1' 
+              placeholder='document' 
+              />
+            {errors.document && (
+              <p className='text-red-500'>{errors.document.message}</p>
+              )}
+          </div>
+
+          <div className='flex w-full flex-col'>
+            <input 
+              {...register('email')} 
+              className='rounded-md px-3 py-1' 
+              placeholder='Email' 
+              />
+            {errors.email && (
+              <p className='text-red-500'>{errors.email.message}</p>
+            )}
+          </div>
+        
 
       <div className='flex gap-2'>
         <div className='flex w-full flex-col'>
@@ -52,6 +87,9 @@ const Form = () => {
             type='date' 
             className='rounded-md px-3 py-1' 
           />
+          {errors.initialDate && (
+            <p className='text-red-500'>{errors.initialDate.message}</p>
+          )}
         </div>
         <div className='flex w-full flex-col'>
           <input 
@@ -59,6 +97,9 @@ const Form = () => {
             type='date' 
             className='rounded-md px-3 py-1' 
           />
+          {errors.finalDate && (
+            <p className='text-red-500'>{errors.finalDate.message}</p>
+          )}
         </div>
       </div>
 
